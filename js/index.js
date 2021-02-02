968a5b93e749a925df7f3b36c721374432f78ac7
Tatsiana Yasiuchenia <tsbarankina@gmail.com>
2021-02-02 16:58:34 +0300

    Закончила форму фильтра и доступность внутренней страницы, проверила на переполнение, прикрутила скрипт и анимацию для модального окна

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-const modalShowButton = document.querySelector(".modal-show-button");
-const modalForm = document.querySelector(".modal-search");
+let modalShowButton = document.querySelector(".modal-show-button");
+let modalForm = document.querySelector(".modal-search");
 
 modalShowButton.addEventListener("click", function (evt) {
     evt.preventDefault();
-    if (modalForm.classList.contains("display-none")) {
-        modalForm.classList.remove("display-none");
-    }) else {modalForm.classList.add("display-none");
-}
+    modalForm.classList.toggle("display-none");
+});
+
+
 
 
 
